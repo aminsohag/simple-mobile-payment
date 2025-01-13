adacb3cbdb00055303ed397305a330f80249da41
aminsohag <aminsohag508@gmail.com>
2025-01-13 16:04:02 +0600

    features js section

--- js/features.js
+++ js/features.js
@@ -1,9 +1,9 @@
-// document.getElementById('cash-out-show-btn').addEventListener('click', function(){
-//     document.getElementById('cash-out-sec').classList.remove('hidden');
-//     document.getElementById('add-money-sec').classList.add('hidden')
-// })
+document.getElementById('cash-out-show-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-sec').classList.remove('hidden');
+    document.getElementById('add-money-sec').classList.add('hidden')
+})
 
-// document.getElementById('add-money-show-btn').addEventListener('click', function(){
-//     document.getElementById('add-money-sec').classList.remove('hidden');
-//     document.getElementById('cash-out-sec').classList.add('hidden')
-// })
+document.getElementById('add-money-show-btn').addEventListener('click', function(){
+    document.getElementById('add-money-sec').classList.remove('hidden');
+    document.getElementById('cash-out-sec').classList.add('hidden')
+})
